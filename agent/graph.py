e4db72b94bd7e93169f067cf5de4e39fc86d66fd
codexsabir <codexsabir@gmail.com>
2025-09-24 15:27:41 +0500

    Fix formatting and add missing newline in graph.py

--- agent/graph.py
+++ agent/graph.py
@@ -1,6 +1,9 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from prompts import *
+
 load_dotenv()
+
 llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0)
 response = llm.invoke("who invented openai?")
-print(response.content) 
+print(response.content)
