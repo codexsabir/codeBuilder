80f813a755e930a75d2744b58531c0a9444c7887
codexsabir <codexsabir@gmail.com>
2025-09-24 15:59:22 +0500

    Implement planner and architect prompts for project planning and task breakdown

--- agent/graph.py
+++ agent/graph.py
@@ -1,9 +1,22 @@
 from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph
 from dotenv import load_dotenv
 from prompts import *
-
+from states import *
 load_dotenv()
 
 llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0)
-response = llm.invoke("who invented openai?")
-print(response.content)
+user_prompt = 'create a simple calculator web application'
+
+def planner_agent(state:dict)-> dict:
+    users_prompt = state['user_prompt']
+    response = llm.with_structured_output(Plan).invoke(planner_prompt(user_prompt))
+    return {"plan":response}
+
+graph = StateGraph(dict)
+graph.add_node('planner',planner_agent)
+graph.set_entry_point('planner')
+agent = graph.compile()
+
+result = agent.invoke({'user_prompt':user_prompt})
+print(result)
