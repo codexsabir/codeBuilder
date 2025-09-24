702552c241be929b03bfde392b33505d22d37358
codexsabir <codexsabir@gmail.com>
2025-09-24 15:20:20 +0500

    Update invocation query in graph.py to ask about OpenAI's inventor

--- agent/graph.py
+++ agent/graph.py
@@ -2,5 +2,5 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 load_dotenv()
 llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0)
-response = llm.invoke("Write a python function that adds two numbers")
-print(response.content)
+response = llm.invoke("who invented openai?")
+print(response.content) 
